803b2d66171e272e6963d9a1ea56308e5686954c
Ivan Xamarin <ivanxamarin032019@hotmail.com>
2021-05-04 18:40:47 +0200

    Crear Proyecto Api y las Entities

---
+++ POSUNO.Api/Data/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace POSUNO.Api.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirsName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
